d2242377f566624a79e327ecee09ce2987089ffa
swathiprojectslearner <swathiprojectslearner@gmail.com>
2026-06-09 19:55:54 +0530

    force ipv4 dns for nodemailer on Render

--- event-backend/utils/sendMail.js
+++ event-backend/utils/sendMail.js
@@ -1,14 +1,19 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const sendMail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host:"smtp.gmail.com",
+    host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    familly: 4, //force Ipv4
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
